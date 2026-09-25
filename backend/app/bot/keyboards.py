4d29c00d4224e9cedfd450d0d9f0a650f2325b75
shadowslik <artiomk00o@gmail.com>
2026-09-25 20:50:01 +0400

    minimal fix

--- backend/app/bot/keyboards.py
+++ backend/app/bot/keyboards.py
@@ -15,5 +15,5 @@ def get_location():
 
 def open_route():
     return [
-        [buttons.LinkButton("Собрать маршрут", "https://max-time-guide.ovoaookaaooaoa.workers.dev")],
+        [buttons.LinkButton("Собрать маршрут", "https://168.113.157.47.sslip.io")],
     ]
